ebc292ce099a7bcac8740e05f794a393e48175ea
SteMuc <stefanoangeli12@gmail.com>
2024-04-27 17:48:21 +0200

    Added my_panda_moveit_config to my_panda_robot

--- my_panda_gazebo/launch/simulation.launch.py
+++ my_panda_gazebo/launch/simulation.launch.py
@@ -20,7 +20,14 @@ from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-   
+    
+    load_gripper = LaunchConfiguration("load_gripper")
+    declare_load_gripper_arg = DeclareLaunchArgument(
+        "load_gripper",
+        default_value="False",
+        description="Use Franka Gripper as end-effector if True",
+    )
+    ## Spawn empty world in ignition-gazebo.
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
@@ -33,9 +40,60 @@ def generate_launch_description():
         ),
         launch_arguments={'gz_args': '-r empty.sdf'}.items(),
     )
-
-    return LaunchDescription(
+    
+    ## Set name of controller configuration file.
+    controller_config_name = PythonExpression(
         [
-            gz_sim
+          "'panda_ros_controllers.yaml'"
+        ]
+    )
+    
+    # Load robot controllers file given the configuration file above.
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("my_panda_controller"),
+            "config",
+            controller_config_name,
+        ]
+    )
+    
+    # Load robot description content that will be used by 'gz_spawn_entity" node below.
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("my_panda_description"),
+                    "robots",
+                    "panda_arm.urdf.xacro",
+                ]
+            ),
+            "hand:=",
+            load_gripper,
+            "use_sim:=True",
+            "simulation_controllers_config_file:=",
+            robot_controllers,
+        ]
+    )
+    
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name",
+            "panda_arm_manipulator",
+            "-allow_renaming",
+            "true",
+            "-string",
+            robot_description_content
+        ],
+        output="screen",
+    )
+    
+    return LaunchDescription(
+        [   declare_load_gripper_arg,
+            gz_sim, ## Spawn empty world in ignition-gazebo.
+            gz_spawn_entity ## Spawn Franka Emika Robot on the table.
         ]
     )
